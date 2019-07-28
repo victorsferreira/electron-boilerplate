5f254cb551bc814694e0e592df7b47bf0f45ebe3
Victor Ferreira <victor.ferreira@deliveryhero.com>
2019-07-28 18:12:23 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('./src/index.html');
+
+  win.on('closed', () => {
+    win = null
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
